1f76606869c1eac7d5f3fbd041cd8e998ba62713
mentuly <danlutsencko@gmail.com>
2024-11-07 18:39:18 +0200

    Co-authored-by: Shalenyi Matvii <SMatvii@users.noreply.github.com> Co-authored-by: Yaromyr <Yar00myr@users.noreply.github.com>

---
+++ app/routes/helpful.py
@@ -0,0 +1,61 @@
+from passlib.context import CryptContext
+import jwt
+from jwt.exceptions import InvalidTokenError
+from typing import Annotated
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends, FastAPI, HTTPException, status
+from uvicorn import run
+from sqlmodel import SQLModel, Field, create_engine
+from sqlalchemy.orm import sessionmaker, Session
+import hashlib
+from ..db.models.token import Token
+from .oauth2 import SECRET_KEY, ALGORITHM, PWD_CONTEXT
+
+
+def verify_password(plain_password, hashed_password):
+    return PWD_CONTEXT.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return PWD_CONTEXT.hash(password)
+
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(OAUTH2_SCHEME)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    with SESSION.begin() as session:
+        user = get_user(session, username=token_data.username)
+        if user is None:
+            raise credentials_exception
+        return User(**user.model_dump())
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
--- app/routes/oauth2.py
+++ app/routes/oauth2.py
@@ -11,28 +11,14 @@ import hashlib
 import jwt
 from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
-from ..db import Token,Author
+from ..db.models.config import Config
+from ..db.models.token import Token
 
-
-app = FastAPI()
 
 SECRET_KEY = "d29b0518d40de3bdc7cd369265bb4ff3daec082666f0923979ae8d44f2c8946f"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-ENGINE = create_engine("sqlite:///mydb.db", echo=True)
-SESSION = sessionmaker(bind=ENGINE)
+
 
 OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="token")
 PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-auth_router = APIRouter(tags=["Authentication"])
-
-
-
-def get_password_hash(password: str) -> str:
-    return PWD_CONTEXT.hash(password)
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return PWD_CONTEXT.verify(plain_password,hashed_password)
- 
- 
