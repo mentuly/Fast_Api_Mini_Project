f5e89396a417872ba67b2bf211e2fd81f2a52e40
Yar00myr <ivanucaromir@gmail.com>
2024-11-09 11:41:16 +0000

    .....

--- app/routes/request.py
+++ app/routes/request.py
@@ -1,24 +1,26 @@
-from fastapi import APIRouter, HTTPException,Depends
-from ..db import ArticleRequest,Article
-from sqlmodel import select,Session
-from ..utils import get_session
 from typing import Annotated
+from fastapi import APIRouter, HTTPException, Depends
+from sqlmodel import select, Session
+from ..db import ArticleRequest, Article
+from ..utils import get_session
 
 
-
-request_router=APIRouter(prefix="/request",tags=["Request"])
-
+request_router = APIRouter(prefix="/request", tags=["Request"])
 
 
 @request_router.post("")
-def request(data:ArticleRequest,session:Annotated[Session,Depends(get_session)]):
-    response=[]
-    keywords=data.keywords
-    end_date=data.date_range.end_date
-    start_date=data.date_range.start_date
-    articles = session.scalars(select(Article).where(Article.published_at>=start_date,Article.published_at<=end_date))
+def request(data: ArticleRequest, session: Annotated[Session, Depends(get_session)]):
+    response = []
+    keywords = data.keywords
+    end_date = data.date_range.end_date
+    start_date = data.date_range.start_date
+    articles = session.scalars(
+        select(Article).where(
+            Article.published_at >= start_date, Article.published_at <= end_date
+        )
+    )
     if not articles:
-        raise HTTPException(status_code=402,detail="No article matches your request")
+        raise HTTPException(status_code=402, detail="No article matches your request")
     for article in articles:
         if any(w in article.content for w in keywords):
             response.append(article)
